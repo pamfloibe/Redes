5bb78e77a9700c0bc26c491994a77a623234cc62
unknown <pamfloibe@gmail.com>
2019-09-30 14:33:16 -0500

    Tarea 3 - Escenario 1. Simulacion completa

--- Tarea4_Redes.m
+++ Tarea4_Redes.m
@@ -1,46 +1,67 @@
-stationA = 0;
-stationB = 0;
-numTransmisionesA = 0;
-numTransmisionesB = 0;
-totalTransmisiones = 0;
-totalIntentos = 0;
-x=0;
+clear all; clc;
+pTransmision= [0.005,	0.01,	0.05,	0.07,	0.1];
+tasaA=zeros(1,5);
+tasaB=zeros(1,5);
 
-numColisiones = 0;
-tiempo = .05; %50 ms
-pTransmision = 0.5; %transmision exitosa
+for i=1:length(pTransmision)
+    stationA = 0;
+    stationB = 0;
+    numTransmisionesA = 0;
+    numTransmisionesB = 0;
+    transmisionesExitosasA=0;
+    transmisionesExistosasB=0;
+    x=0;
+    y=0;
+    numColisiones = 0;
 
-while numColisiones < 50 %Minimo # de colisiones
-    totalIntentos = totalIntentos + 1; %# de intentos de transmision
-    
-    x=rand; %probabilidad de que transmita A
-    if(x <= pTransmision) %Si es menor, se manda
-        stationA = 1; %se transmite A
-        numTransmisionesA = numTransmisionesA + 1;
-    else 
-        stationA = 0; %no se transmite A
+    while numColisiones <= 50 %Minimo # de colisiones
+
+        x=rand; %probabilidad de que transmita A
+        if(x <= pTransmision(i)) %Si es menor, se manda
+            stationA = 1; %se transmite A
+            numTransmisionesA = numTransmisionesA + 1;
+        else 
+            stationA = 0; %no se transmite A
+        end
+
+
+        y=rand; %probabilidad de que transmita B
+         if(y <= pTransmision(i)) %Si es menor, se manda
+            stationB = 1; %se transmite B
+            numTransmisionesB = numTransmisionesB + 1;
+         else 
+            stationB = 0; %no se transmite B
+         end
+
+         %Revisar si hay colisiones
+         if(stationA == 1 && stationB == 1)
+            numColisiones = numColisiones + 1;
+         end 
     end
+
+    transmisionesExitosasA=numTransmisionesA-numColisiones;
+    transmisionesExitosasB=numTransmisionesB-numColisiones;
     
-    
-    x=rand; %probabilidad de que transmita B
-     if(x <= pTransmision) %Si es menor, se manda
-        stationB = 1; %se transmite B
-        numTransmisionesB = numTransmisionesB + 1;
-     else 
-        stationB = 0; %no se transmite B
-     end
-     
-     
-     %Revisar numero de transmisiones
-     if(stationA == 1 || stationB == 1)
-         totalTransmisiones = totalTransmisiones + 1;
-     end
-     
-     %Revisar si hay colisiones
-     if(stationA == 1 && stationB == 1)
-        numColisiones = numColisiones + 1;
-     end
-    
+    tasaA(i)=transmisionesExitosasA/numTransmisionesA;
+    tasaB(i)=transmisionesExitosasB/numTransmisionesB;
 end
 
-tiempoTransmision = totalIntentos*tiempo;
+figure (1)
+%interpolacion sobre los valores obtenidos
+plot(pTransmision, tasaA);
+hold on;
+%valores obtenidos
+plot(pTransmision, tasaA,'*');
+title('Tasa de transmisiones exitosas en A');
+xlabel('Probabilidad de que A transmita una trama');  
+ylabel('Tasa de éxito'); 
+
+figure (2)
+%interpolacion sobre los valores obtenidos
+plot(pTransmision, tasaB);
+hold on;
+%valores obtenidos
+plot(pTransmision, tasaB,'*');
+title('Tasa de transmisiones exitosas en B');
+xlabel('Probabilidad de que B transmita una trama');
+ylabel('Tasa de éxito');
